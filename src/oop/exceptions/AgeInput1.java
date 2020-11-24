37eb8f21c16392d231413036a498f0e453963292
ardhiesta <ardhi.mail@gmail.com>
2020-11-25 03:24:22 +0700

    remove Scanner var, add scanner as variable inside getAge

--- src/oop/exceptions/AgeInput1.java
+++ src/oop/exceptions/AgeInput1.java
@@ -4,19 +4,10 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
 
+// program mencari tahun lahir berdasarkan usia
+
 public class AgeInput1 {
-	private static final String DEFAULT_MESSAGE = "Your age: ";
-	private Scanner scanner;
-
-	public AgeInput1() {
-		scanner = new Scanner(System.in);
-	}
-
-	public int getAge() {
-		return getAge(DEFAULT_MESSAGE);
-	}
-
-	public int getAge(String prompt) {
+	public int getAge(String prompt, Scanner scanner) {
 		System.out.print(prompt);
 		int age = scanner.nextInt();
 		return age;
@@ -26,12 +17,14 @@ public class AgeInput1 {
 		GregorianCalendar today;
 		int age, thisYear, bornYr;
 		String answer;
+		
 		Scanner scanner = new Scanner(System.in);
 		AgeInput1 input = new AgeInput1();
-		age = input.getAge("How old are you? ");
+		age = input.getAge("How old are you? ", scanner);
 		today = new GregorianCalendar();
 		thisYear = today.get(Calendar.YEAR);
 		bornYr = thisYear - age;
+		
 		System.out.print("Already had your birthday this year? (Y or N)");
 		answer = scanner.next();
 		if (answer.equals("N") || answer.equals("n")) {
