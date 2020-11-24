ce5904e218fa02005154e62d9862ae144f24d042
ardhiesta <ardhi.mail@gmail.com>
2020-11-25 03:39:45 +0700

    remove Scanner var, add scanner as variable inside getAge

--- src/oop/exceptions/AgeInput3.java
+++ src/oop/exceptions/AgeInput3.java
@@ -6,18 +6,18 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class AgeInput3 {
-	private static final String DEFAULT_MESSAGE = "Your age: ";
-	private Scanner scanner;
+//	private static final String DEFAULT_MESSAGE = "Your age: ";
+//	private Scanner scanner;
+//
+//	public AgeInput3() {
+//		scanner = new Scanner(System.in);
+//	}
+//
+//	public int getAge() {
+//		return getAge(DEFAULT_MESSAGE);
+//	}
 
-	public AgeInput3() {
-		scanner = new Scanner(System.in);
-	}
-
-	public int getAge() {
-		return getAge(DEFAULT_MESSAGE);
-	}
-
-	public int getAge(String prompt) {
+	public int getAge(String prompt, Scanner scanner) {
 		int age = 0;
 		while (true) {
 			System.out.print(prompt);
@@ -41,12 +41,14 @@ public class AgeInput3 {
 		GregorianCalendar today;
 		int age, thisYear, bornYr;
 		String answer;
+		
 		Scanner scanner = new Scanner(System.in);
 		AgeInput3 input = new AgeInput3();
-		age = input.getAge("How old are you? ");
+		age = input.getAge("How old are you? ", scanner);
 		today = new GregorianCalendar();
 		thisYear = today.get(Calendar.YEAR);
 		bornYr = thisYear - age;
+		
 		System.out.print("Already had your birthday this year? (Y or N)");
 		answer = scanner.next();
 		if (answer.equals("N") || answer.equals("n")) {
